4bf56e3c069b3e8a953cc706263cb129d14a3d2c
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-08 22:45:47 +0700

    Problem 8

--- Assets/Scripts/Problem6.cs
+++ Assets/Scripts/Problem6.cs
@@ -9,17 +9,39 @@ public class Problem6 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int objectCount = Random.Range(5, 10);
-        for(int i = 0; i < objectCount; i++)
+        
+        int objectCount = 5;
+        for (int i = 0; i < objectCount; i++)
         {
             float randomX = Random.Range(minX, maxX);
             float randomY = Random.Range(minY, maxY);
-            Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity);
+
+            GameObject Meteor = Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
+            Meteor.transform.SetParent(GameObject.FindGameObjectWithTag("Spawn").transform, false);
         }
+        StartCoroutine(SpawnKotak());
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+        
+    }
+    IEnumerator SpawnKotak()
+    {
+        if (transform.childCount < 5)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+
+            GameObject Meteor = Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
+            Meteor.transform.SetParent(GameObject.FindGameObjectWithTag("Spawn").transform, false);
+        }
+        yield return new WaitForSeconds(3);
+        StartCoroutine(SpawnKotak());
+    }
+    private void OnGUI()
     {
         
     }
