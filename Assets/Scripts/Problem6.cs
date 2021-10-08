4a7d2934bca81bfb9de07fe7031c7f5214c2f48d
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-08 21:46:50 +0700

    Problem 6

---
+++ Assets/Scripts/Problem6.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem6 : MonoBehaviour
+{
+    public float minX, maxX, minY, maxY;
+    public GameObject kotakPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int objectCount = Random.Range(5, 10);
+        for(int i = 0; i < objectCount; i++)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+            Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
